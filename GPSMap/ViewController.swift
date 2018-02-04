9755576b28d264b07f38b84cb6e9b7ee31efe15a
Erik Lindberg <e.lindberg@gmail.com>
2018-02-03 16:11:01 -0800

    Initial Commit

---
+++ GPSMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GPSMap
+//
+//  Created by Erik Lindberg on 2018-02-03.
+//  Copyright © 2018 Erik Lindberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
